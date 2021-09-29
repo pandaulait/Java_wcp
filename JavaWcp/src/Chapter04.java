03239f65db010b3dff7108d4ec3c88aa1777ad47
pandaulait <show.hey.panda@gmail.com>
2021-09-29 16:54:23 +0900

    [Finish]Chapter4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String my_name = "pandaulait";
+		final int LUCKY_NUMBER = 7;
+        System.out.println(my_name);
+        my_name = "人は考える葦である。";
+        System.out.println(my_name);
+        System.out.println(LUCKY_NUMBER);
+	}
+}
