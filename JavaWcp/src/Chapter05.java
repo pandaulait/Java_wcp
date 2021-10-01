d74bc5c55ad0dfa7d65e93aebe3b77413191d582
pandaulait <show.hey.panda@gmail.com>
2021-10-01 23:11:00 +0900

    [Finish]Chapter05

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,25 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long l = 123456789L;
+		System.out.println(l);
+		double d = 3.14;
+		System.out.println(d);
+		boolean b = true;
+		System.out.println(b);
+//		int i1 = null;
+//		System.out.println(i1);
+				
+        Integer i = 100;
+        System.out.println(i);
+        Float f = 1.4142f;
+        System.out.println(f);
+        Character c = 'う';
+        System.out.println(c);
+		Integer i2 = null;
+		System.out.println(i2);
+
+        int[] array = { 0, 10, 20, 30, 40 };
+        System.out.println(array[3]);
+	}
+}
